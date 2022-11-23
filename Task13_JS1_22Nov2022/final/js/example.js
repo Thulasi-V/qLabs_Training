834cbbb23ec3706d5d4428cc8fc446915d955411
Thulasi-V <117075107+Thulasi-V@users.noreply.github.com>
2022-11-23 08:38:10 +0530

    Add files via upload

--- Task13_JS1_22Nov2022/final/js/example.js
+++ Task13_JS1_22Nov2022/final/js/example.js
@@ -5,7 +5,20 @@
 // prompt('Enter you name');
 //confirm popup box//
 
-// var validation= confirm("Enter Coupon code");
+var validation= confirm("Enter Coupon code");
+console.log(validation)
+
+if(validation==true){
+   
+    alert("Coupon applied Sucessfully");
+}
+else{
+    
+    alert("Please enter valid coupon code");
+}
+
+//prompt popup box//
+// var validation= prompt("Enter Coupon code");
 // console.log(validation)
 
 // if(validation==true){
@@ -18,13 +31,20 @@
 // }
 
 
+
+
+
+
+
+
+
 //prompt popup box//
-var username=prompt("Enter username :  ")
-console.log(username)
-if(username != ' '&& username!=null){
-    alert("hello"+username)
-}
-else{
-    alert("Enter your username")
-}
+// var username=prompt("Enter username :  ")
+// console.log(username)
+// if(username != ' '&& username!=null){
+//     alert("hello"+username)
+// }
+// else{
+//     alert("Enter your username")
+// }
 
