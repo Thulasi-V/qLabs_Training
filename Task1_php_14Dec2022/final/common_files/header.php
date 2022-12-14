a103e2ea1a107165c791dc862def5f680da40f86
Thulasi-V <117075107+Thulasi-V@users.noreply.github.com>
2022-12-14 23:00:00 +0530

    Add files via upload

---
+++ Task1_php_14Dec2022/final/common_files/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forms Series<?php echo $title;?></title>
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css">
+</head>
+<body>
